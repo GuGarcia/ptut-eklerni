e0d59c8d0e1e2401fe559e2427e86a26ee31b165
Cindy <cindy.perat@gmail.com>
2014-03-20 11:50:35 +0100

    Modification classes EcoleType, ClasseType, EnseignantType, EleveType, SerieType (buildForm) + ajout fichier forms.xml contenant les services associés aux formulaires

--- src/Eklerni/CASBundle/Controller/DefaultController.php
+++ src/Eklerni/CASBundle/Controller/DefaultController.php
@@ -4,10 +4,9 @@ namespace Eklerni\CASBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
+class DefaultController extends Controller {
+
+    public function indexAction() {
         return $this->render('EklerniCASBundle:Default:index.html.twig');
     }
 }
