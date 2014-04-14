98fa2a250a979875273751fc0c1cfbbcc168d5eb
Verlhac Gaëtan <viviengaetan69@gmail.com>
2014-04-14 15:47:18 +0200

    Add extension for date taking care of locale.

---
+++ src/Eklerni/BackBundle/Twig/EklerniBackExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Eklerni\BackBundle\Twig;
+
+class EklerniBackExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            'datelocale' => new Twig_Filter_Method($this, 'datelocale')
+        );
+    }
+
+    public function datelocale($d, $format = "%d %B %Y")
+    {
+        if ($d instanceof \DateTime) {
+            $d = $d->getTimestamp();
+        }
+
+        return strftime($format, $d);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'erklerni_back_ext';
+    }
+}
