420c529942fed45164ba990d64e50dc079325e16
Verlhac Gaëtan <viviengaetan69@gmail.com>
2014-04-14 17:01:47 +0200

    Add iCheck files. Move javascript in a separated file. Serie added with an an Activite.

--- src/Eklerni/BackBundle/Controller/ExerciceController.php
+++ src/Eklerni/BackBundle/Controller/ExerciceController.php
@@ -2,14 +2,13 @@
 
 namespace Eklerni\BackBundle\Controller;
 
-use Eklerni\DatabaseBundle\Entity\Matiere;
 use Eklerni\DatabaseBundle\Entity\Activite;
 use Eklerni\DatabaseBundle\Entity\Serie;
-use Eklerni\DatabaseBundle\Entity\Enseignant;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class ExerciceController extends Controller {
+class ExerciceController extends Controller
+{
 
     /**
      * @return \Symfony\Component\HttpFoundation\Response
@@ -19,35 +18,51 @@ class ExerciceController extends Controller {
         // Lister matière
         $matieres = $this->get('eklerni.manager.matiere')->findAll();
 
-        return $this->render('EklerniBackBundle:Exercice:index.html.twig', array(
-                "title"     => $this->get('translator')->trans("title.exercice"),
-                "matieres"  => $matieres,
+        return $this->render(
+            'EklerniBackBundle:Exercice:index.html.twig',
+            array(
+                "title" => $this->get('translator')->trans("title.exercice"),
+                "matieres" => $matieres,
             )
         );
     }
 
-    public function ajouterAction(Request $request) {
+    public function ajouterAction(Request $request, $idActivite)
+    {
+        /** @var Activite $activite */
+        $activite = $this->get('eklerni.manager.activite')->findById($idActivite)[0];
+
+        if (!$activite) {
+            throw new \Exception($this->get('translator')->trans('activite.notfound'));
+        }
+
         $serie = new Serie();
         /** @var \Eklerni\DatabaseBundle\Entity\Enseignant $enseignant */
         $enseignant = $this->get("security.context")->getToken()->getUser();
-        $form = $this->createForm('eklerni_serie', $serie);
 
+        $form = $this->createForm('eklerni_serie', $serie);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
+            $serie->setActivite($activite);
             $serie->setEnseignant($enseignant);
             $enseignant->addSerie($serie);
+
             $this->get('eklerni.manager.serie')->save($serie, true);
+            return $this->redirect($this->generateUrl('eklerni_back_exercice'));
         }
 
-        return $this->render('EklerniBackBundle:Exercice:ajouter.html.twig', array(
-                'form'  => $form->createView(),
+        return $this->render(
+            'EklerniBackBundle:Exercice:ajouter.html.twig',
+            array(
+                'form' => $form->createView(),
                 'title' => $this->get('translator')->trans("title.create_serie"),
             )
         );
     }
 
-    public function listerMatiereAction() {
+    public function listerMatiereAction()
+    {
 
     }
 } 
--- web/js/global.js
+++ web/js/global.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $(".masked_date").inputmask("yyyy-mm-dd", {"placeholder": "yyyy-mm-dd"});
+    $("input[type='checkbox'], input[type='radio']").iCheck({
+        checkboxClass: 'icheckbox_minimal',
+        radioClass: 'iradio_minimal'
+    });
 });
