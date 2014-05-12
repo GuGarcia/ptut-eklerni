a335f4839618b5e5a111873315c157a374444917
Cindy Perat <cindy.perat@live.fr>
2014-05-12 10:57:22 +0200

    Modification élève

--- web/js/global.js
+++ web/js/global.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".masked_date").inputmask("yyyy-mm-dd", {"placeholder": "yyyy-mm-dd"});
+    $(".masked_date").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
     $("input[type='checkbox'], input[type='radio']").iCheck({
         checkboxClass: 'icheckbox_minimal',
         radioClass: 'iradio_minimal'
